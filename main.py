0adeea36a63ec6a20e29dce00f450c249b58c4cb
vasiliyvn <mishadasha1114@gmail.com>
2024-07-22 13:55:12 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+sort=sorted(students)
+grades_a=[]
+for num in grades:
+    s=sum(num)/len(num)
+    grades_a.append(s)
+dict1=dict(zip(sort,grades_a))
+print(dict1)
